5fd93b2b64db7ca4e0c250122ae9cfab78f10a57
kojwangbora <kojwangbora254@gmail.com>
2022-02-06 02:31:53 +0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Goal } from './goal';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+// export class AppComponent {
+//   goals:string[];
+//   constructor(){
+//     this.goals=[ 'Watch finding Nemo', 'Buy cookies', 'Get new phone case']
+//   }
+// }
+export class AppComponent{
+   title = 'Goals';
+}
